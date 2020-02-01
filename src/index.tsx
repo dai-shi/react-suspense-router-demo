cfb6680d5eb9fe4a10a778ba65492ab31f7f2a18
daishi <daishi@axlight.com>
2020-02-01 23:48:21 +0900

    import createRoot directly

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,14 @@
-// eslint-disable-next-line spaced-comment
 /// <reference types="react-dom/experimental" />
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 const ele = document.getElementById('root');
 if (!ele) throw new Error('no root');
-ReactDOM.createRoot(ele).render(<App />);
+createRoot(ele).render(<App />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
