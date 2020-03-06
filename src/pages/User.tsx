2bf2561fe42c20e4643087f4184de5da834f2f45
daishi <daishi@axlight.com>
2020-03-06 21:06:57 +0900

    split User component

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -3,21 +3,33 @@ import { useRouteData, Link } from "react-suspense-router";
 
 import { UserData } from "./User.data";
 
-const User: React.FC = () => {
+const UserID: React.FC = () => {
   const userData = useRouteData() as UserData;
-  return (
-    <div>
-      <h1>User</h1>
-      <ul>
-        <li>User ID: {userData.data.id}</li>
-        <li>First Name: {userData.data.first_name}</li>
-        <li>Last Name: {userData.data.last_name}</li>
-      </ul>
-      <Link to="/user/-1">
-        Go to Page that raises an error (User ID: -1)
-      </Link>
-    </div>
-  );
+  return <li>User ID: {userData.data.id}</li>;
 };
+
+const FirstName: React.FC = () => {
+  const userData = useRouteData() as UserData;
+  return <li>First Name: {userData.data.first_name}</li>;
+};
+
+const LastName: React.FC = () => {
+  const userData = useRouteData() as UserData;
+  return <li>Last Name: {userData.data.last_name}</li>;
+};
+
+const User: React.FC = () => (
+  <div>
+    <h1>User</h1>
+    <ul>
+      <UserID />
+      <FirstName />
+      <LastName />
+    </ul>
+    <Link to="/user/-1">
+      Go to Page that raises an error (User ID: -1)
+    </Link>
+  </div>
+);
 
 export default User;
